5f9f1bd29abf96e174203c605a34b82a651e7f53
lrw <dlsodnd123@naver.com>
2020-12-17 17:49:28 +0900

    네이버예제 내용 수정 및 추가

--- js/rolling.js
+++ js/rolling.js
@@ -5,20 +5,20 @@ var rollingTimesquareInterval
 function startRollingNews(){
     rollingNewsInterval = setInterval(() => {
         if(!$('.rolling-news .news').is(':animated')){
-            $('.rolling-news .news').first().animate({'margin-top':'-24px'},900,function(){
+            $('.rolling-news .news').first().animate({'margin-top':'-24px'},500,function(){
                 $(this).detach().appendTo('.rolling-news').removeAttr('style');
             })
         }
-    }, 1000);
+    }, 3000);
 }
 function startRollingTimesquareInterval(){
     rollingTimesquareInterval = setInterval(() => {
         if(!$('.timesquare-rolling .card').is(':animated')){
-            $('.timesquare-rolling .card').first().animate({'margin-left':'-281px'},1500,function(){
+            $('.timesquare-rolling .card').first().animate({'margin-left':'-281px'},300,function(){
                 $(this).detach().appendTo('.timesquare-rolling').removeAttr('style');
             })
         }
-    }, 2000);
+    }, 3500);
 }
 
 $(function(){
@@ -30,7 +30,7 @@ $(function(){
     },function(){
         startRollingNews();
     })
-    
+
     $('.timesquare-card').hover(function(){
         clearInterval(rollingTimesquareInterval);
     },function(){
